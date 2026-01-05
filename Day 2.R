8113fe0f388772abd7cf1b60f025db476e8ebec0
Barney-Dawes <barney.dawes@defra.gov.uk>
2026-01-05 15:17:27 +0000

    Finished day 2, with workings

--- Day 2.R
+++ Day 2.R
@@ -86,22 +86,26 @@ answer_1 <- invalid_IDs %>%
 #### Part 2 ####
 
 
-#function to compare multiple numbers together to see if they're the same
-num_comp <- function(vector){
-  
-  #predefine the length of the output vector for speed
-  output <- vector(length = length(vector))
-  
-  #for each element of the vector we check whether it's the same value as the first
-  for (i in 1:length(vector)){
-    if (vector[1] == vector[i]){
-      output[i] <- TRUE
-    }
-  }
-  
-  #if all the elements have the same value as the first, then all elements are equal
-  return(all(output))
-}
+# #function to compare multiple numbers together to see if they're the same
+# num_comp <- function(vector){
+#   
+#   #predefine the length of the output vector for speed
+#   output <- vector(length = length(vector))
+#   
+#   #for each element of the vector we check whether it's the same value as the first
+#   for (i in 1:length(vector)){
+#     if (vector[1] == vector[i]){
+#       output[i] <- TRUE
+#     
+#     #if even one of the pairs don't match then we know that the whole vector isn't a series of repeated sections
+#     } else {
+#       break
+#     }
+#   }
+#   
+#   #if all the elements have the same value as the first, then all elements are equal
+#   return(all(output))
+# }
 
 
 
@@ -109,46 +113,59 @@ num_comp <- function(vector){
 #creating a new version of our pattern checking function to now detect when there's any type of repeated pattern, i.e. 555 would now be considered an invalid ID
 pattern_check2 <- function(x){
   
+  # input type
+  #x <- 10:12
+  
   #creating the vector where we'll store our answers at the start, rather than building it incrementally, for speed
   output <- rep(FALSE, times = length(x))
   
   for (i in 1:length(x)){
-    nchars <- floor(log10(x[i])) + 1
-    
-    #finding the prime factorisation of nchars
-    primes <- primeFactors(nchars) %>% unique()
+
+    # #finding the prime factorisation of nchars
+    # primes_of_nchars <- primeFactors(nchars) %>% unique()
     
     #extracting the digits of the number
-    digits <- strsplit(as.character(x[i]), "") %>% unlist
+    digits <- strsplit(as.character(x[i]), "") %>% unlist %>% as.numeric()
     
-    #first check if it's a single digit repeated
-    if (length(unique(digits)) == 1){
+    nchars <- length(digits)
+    
+    #first check if it's a single digit repeated or an integer between 1-9 inclusive
+    if (length(unique(digits)) == 1 && nchars > 1){
       output[i] <- TRUE
+      #return(TRUE) or output <- TRUE
+      
     } else {
       
-      #checking for repeated patterns of all possible lengths based off the prime factors
-      for (j in 1:length(primes)){
+      #work out how many possible sections there could be, i.e. with 6 digits they could be sectioned in twos or threes
+      divisors <- divisors(nchars)
+      divisors <- divisors[-c(1,length(divisors))] #removing 1 and 'nchars'
+      
+      #looping over each divisor
+      for (divs in divisors){
         
-        #looping over the length of the prime we're on to compare digits
-        for (k in 1:length(primes[j])){
+        #keeping track of the successes and failures we have
+        tracker <- rep(FALSE, times = divs - 1)
+        
+        #looping over the sections of the digits for a given divisor
+        for (sections in 2:divs){
           
+          #check to see that the selected sections digits match the first sections - working on the counter factual for speed as we expect most numbers to be valid
+          if (any(digits[1:(nchars/divs)] != digits[((nchars * (sections - 1)/divs) + 1) : (nchars * sections/divs)])) {
+            break
+          } else {
+            tracker[sections - 1] <- TRUE
+          }
           
         }
         
-        
+        #checking that for our current divisor, all sections are equal, if so we record this and skip any remaining divisors as they're unnecessary to check
+        if (all(tracker)) {
+          output[i] <- TRUE
+          break
+        } 
       }
       
-      
     }
-    
-    
-    
-    
-    
-    
-    
-    
-
     
   }
   
@@ -157,15 +174,26 @@ pattern_check2 <- function(x){
 }
 
 
+#creating a list to store invalid IDs in
+invalid_IDs <- vector("list", length = nrow(split_data))
 
 
+for (i in 1:nrow(split_data)){
+  
+  #checking how far through the ranges of product IDs we are
+  print(paste0("Checking row ", i, " of ", nrow(split_data), " rows"))
+  
+  #creating the range which will be the functions input
+  range <- split_data$First[i]:split_data$Last[i]
+  
+  #finding the invalid IDs for that range
+  invalid_IDs_temp <- range[pattern_check2(range)]
+  
+  #storing results in the list
+  invalid_IDs[[i]] <- invalid_IDs_temp
+}
 
-
-
-
-
-
-
-
-
+answer_2 <- invalid_IDs %>% 
+  unlist() %>% 
+  sum()
 
